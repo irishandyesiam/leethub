0b7081cd98c302d2fe90a1f13d5c7a175fc2ab42
Nathan Irish <110686683+irishandyesiam@users.noreply.github.com>
2023-05-02 21:29:26 -0500

    Time: 54 ms (70.20%), Space: 13.2 MB (89.14%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -9,6 +9,10 @@ class Solution(object):
 #         compare to each other
 #         if same (true) return "x is a palindrome"
 #         else: false
-        return False if x < 0 else x == int(str(x)[::-1])
-            
-            
+        # return False if x < 0 else x == int(str(x)[::-1])
+        # num = str(x)[::1]
+        # print(num)
+        x_str = str(x)
+        if x < 0:
+            return False
+        return x_str == x_str[::-1]
